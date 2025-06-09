feea7cc745ce4d2b10a20b7c4dc2b60ad6c2fc77
44mira <legolaslada@gmail.com>
2025-06-09 16:02:08 +0800

    feat: Sprite model

--- pokedex/models/sprite.py
+++ pokedex/models/sprite.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class Sprite(models.Model):
+    front_default = models.URLField()
+    front_shiny = models.URLField()
+    front_female = models.URLField()
+    front_shiny_female = models.URLField()
+    back_default = models.URLField()
+    back_female = models.URLField()
+    back_shiny_female = models.URLField()
