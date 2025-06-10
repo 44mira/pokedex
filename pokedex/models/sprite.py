6c5b17c62a8e2602fe7574ed77350482b3947477
44mira <legolaslada@gmail.com>
2025-06-10 16:53:15 +0800

    feat: make all sprite fields nullable

--- pokedex/models/sprite.py
+++ pokedex/models/sprite.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Sprite(models.Model):
-    front_default = models.URLField()
-    front_shiny = models.URLField()
-    front_female = models.URLField()
-    front_shiny_female = models.URLField()
-    back_default = models.URLField()
-    back_female = models.URLField()
-    back_shiny_female = models.URLField()
+    front_default = models.URLField(null=True)
+    front_shiny = models.URLField(null=True)
+    front_female = models.URLField(null=True)
+    front_shiny_female = models.URLField(null=True)
+    back_default = models.URLField(null=True)
+    back_female = models.URLField(null=True)
+    back_shiny_female = models.URLField(null=True)
