d6dec5cd94077c9ae896d39a545c4d1e838036ab
44mira <legolaslada@gmail.com>
2025-06-11 13:40:26 +0800

    feat: register admin fields

--- pokedex/admin.py
+++ pokedex/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from pokedex.models import Pokemon, Poketype, Move, Sprite, EvolutionNode, EvolutionTree
+
+
+class EvolutionNodeAdmin(admin.ModelAdmin):
+    list_display = ["pokemon", "parent"]
+
+
 # Register your models here.
+
+admin.site.register(Pokemon)
+admin.site.register(Poketype)
+admin.site.register(Sprite)
+admin.site.register(Move)
+admin.site.register(EvolutionNode, EvolutionNodeAdmin)
+admin.site.register(EvolutionTree)
